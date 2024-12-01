c596bd4f63f6f0ff89ea32dddbc55a4d230a8a2a
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2024-12-01 07:43:59 +0000

    feat: add aoc1

--- solutions/aoc1/aoc1.go
+++ solutions/aoc1/aoc1.go
@@ -23,9 +23,17 @@ func minFind(arr []int) int {
 	return minIndex
 }
 
-func main() {
-	log.Println("start aoc1")
+func countFind(arr []int, ele int) int {
+	count := 0
+	for _, num1 := range arr {
+	   if ele == num1 {
+		  count += 1
+	   }
+	}
+	return count
+}
 
+func step1() {
 	total := 0
 	var left []int
 	var right []int
@@ -73,4 +81,49 @@ func main() {
 	}
 
 	log.Println("total == ", total)
+}
+
+func step2() {
+	total := 0
+	var left []int
+	var right []int
+
+	inputs := utils.ReadFile("../../inputs/aoc1.txt")
+	data := strings.Split(inputs, "\n")
+
+	for index, ele := range data {
+		log.Println(index, " - ", ele)
+		locations := strings.Split(ele, " ")
+
+		log.Println("loc1 ", locations[0])
+		loc1, err1 := strconv.Atoi(locations[0])
+		if err1 != nil {
+			log.Println("err1 conv ", loc1)
+		}
+
+		log.Println("loc2 ", locations[3])
+		loc2, err2 := strconv.Atoi(locations[3])
+		if err2 != nil {
+			log.Println("err2 conv ", loc2)
+		}
+
+		left = append(left, loc1)
+		right = append(right, loc2)
+	}
+
+	for _, ele := range left {
+		occur := countFind(right, ele)
+
+		total += (ele * occur)
+	}
+
+	log.Println("total == ", total)
+}
+
+func main() {
+	log.Println("start aoc1")
+
+	step1()
+
+	step2()
 }
