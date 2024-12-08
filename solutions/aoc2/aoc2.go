e76008de0f4e91a8dc767fc19e7d63b2e0cac679
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2024-12-08 10:54:31 +0000

    feat: add aoc2

--- solutions/aoc2/aoc2.go
+++ solutions/aoc2/aoc2.go
@@ -7,33 +7,36 @@ import (
 	"strconv"
 )
 
-func checkElementsOrder(report string, increasing bool) bool {
+func checkElementsOrder(report string, increasing bool, idxIgnore int) bool {
 	previous := 0
 	elements := strings.Split(report, " ")
 	for index2, ele2 := range elements {
 		value, _ := strconv.Atoi(ele2)
-		if (index2 > 0) {
-			if (increasing && previous < value) || ((!increasing) && previous > value){
+		if (index2 > 0) &&
+			index2 != idxIgnore {
+			if (increasing && previous < value) || 
+					((!increasing) && previous > value) {
 				//log.Println("compare ", previous, " and ", value, " true", " with index ", index2)
 				previous = value
 			} else {
 				//log.Println("compare ", previous, " and ", value, " false", " with index ", index2)
 				return false
 			}
-		} else {
+		} else if (index2 == 0) {
 			previous = value
 		}
 	}
 	return true
 }
 
-func checkElementsOrderDiff(report string) bool {
+func checkElementsOrderDiff(report string, idxIgnore int) bool {
 	previous := 0
 	elements := strings.Split(report, " ")
 	diff_ := [...]int{1, 2, 3}
 	for index2, ele2 := range elements {
 		value, _ := strconv.Atoi(ele2)
-		if (index2 > 0) {
+		if (index2 > 0) &&
+			index2 != idxIgnore {
 			vdiff := 0
 			if (previous > value) {
 				vdiff = previous - value
@@ -45,7 +48,7 @@ func checkElementsOrderDiff(report string) bool {
 			} else {
 				return false
 			}
-		} else {
+		} else if (index2 == 0) {
 			previous = value
 		}
 	}
@@ -69,21 +72,21 @@ func step1() {
 	total := 0
 
 	for _, ele := range data {
-		report := strings.Split(ele, " ")
-		log.Println("analyzing report ", ele)
+		//report := strings.Split(ele, " ")
+		//log.Println("analyzing report ", ele)
 
-		isInc := checkElementsOrder(ele, true)
+		isInc := checkElementsOrder(ele, true, -1)
 		//log.Println(report, " is inc ", isInc)
-		isDec := checkElementsOrder(ele, false)
+		isDec := checkElementsOrder(ele, false, -1)
 		//log.Println(report, " is dec ", isDec)
-		isOrderedDiff := checkElementsOrderDiff(ele)
+		isOrderedDiff := checkElementsOrderDiff(ele, -1)
 
 		isOrdered := isInc || isDec
 		//log.Println(report, " is ordered ", isOrdered)
 		//log.Println(report, " is ordered diff", isOrderedDiff)
 
 		isSafe := isOrdered && isOrderedDiff
-		log.Println(report, " is safe ", isSafe)
+		//log.Println(report, " is safe ", isSafe)
 
 		if (isSafe) {
 			total += 1
@@ -93,7 +96,68 @@ func step1() {
 	log.Println("end solving ", total)
 }
 
+func step2() {
+	log.Println("start solving")
+	inputs := utils.ReadFile("../../inputs/aoc2.txt")
+	data := strings.Split(inputs, "\n")
+
+	total := 0
+
+	for _, ele := range data {
+		//report := strings.Split(ele, " ")
+		//log.Println("analyzing report ", ele)
+
+		isInc := checkElementsOrder(ele, true, -1)
+		//log.Println(report, " is inc ", isInc)
+		isDec := checkElementsOrder(ele, false, -1)
+		//log.Println(report, " is dec ", isDec)
+		isOrderedDiff := checkElementsOrderDiff(ele, -1)
+
+		isOrdered := isInc || isDec
+		//log.Println(report, " is ordered ", isOrdered)
+		//log.Println(report, " is ordered diff", isOrderedDiff)
+
+		isSafe := isOrdered && isOrderedDiff
+		//log.Println(report, " is safe ", isSafe)
+
+		if (isSafe) {
+			total += 1
+		} else {
+			for idx, _ := range strings.Split(ele, " ") {
+				//log.Println("check idx ", idx)
+				//report := strings.Split(ele, " ")
+				//log.Println("analyzing report ", ele)
+
+				isInc := checkElementsOrder(ele, true, idx)
+				//log.Println(report, " is inc ", isInc)
+				isDec := checkElementsOrder(ele, false, idx)
+				//log.Println(report, " is dec ", isDec)
+				isOrderedDiff := checkElementsOrderDiff(ele, idx)
+
+				isOrdered := isInc || isDec
+				//log.Println(report, " is ordered ", isOrdered)
+				//log.Println(report, " is ordered diff", isOrderedDiff)
+
+				isSafe := isOrdered && isOrderedDiff
+				//log.Println(report, " is safe ", isSafe)
+
+				if (isSafe) {
+					//log.Println("good idx ", idx)
+					total += 1
+					break
+				}
+			}
+		}
+		//log.Println("is ", total)
+	}
+
+	log.Println("end solving ", total)
+}
+
 func main() {
     log.Println("solve step1")
 	step1()
+
+	log.Println("solve step2")
+	step2()
 }
